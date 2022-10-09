387532044910d08e5a644a86790abe90d891afc4
Prince Priyadarshi <prince.priyadarshi@beatoapp.com>
2022-10-10 01:42:12 +0530

    first_commit

--- PythonDSA/ARRAYS.py
+++ PythonDSA/ARRAYS.py
@@ -24,3 +24,32 @@ def question1():
 
     return e
 
+# Q2 Find the majority element in the array. A majority element in an array 
+# A[] of size n is an element that appears more than n/2 times (and hence there is at most one such element). 
+
+# Examples : 
+
+# Input : {3, 3, 4, 2, 4, 4, 2, 4, 4}
+# Output : 4
+# Explanation: The frequency of 4 is 5 which is greater than the half of the size of the array size. 
+
+# Input : {3, 3, 4, 2, 4, 4, 2, 4}
+# Output : No Majority Element
+# Explanation: There is no element whose frequency is greater than the half of the size of the array size.
+
+def question2():
+    a=input()
+    a=a.split(",")
+    c=[]
+    d={}
+    for i in a:
+        c.append(int(i))
+    for i in c:
+        d[i]=c.count(i)
+    if max(d.values()) > len(d) :
+        e=max(zip(d.values(), d.keys()))[1]
+    else:
+        e="No Majority Element"
+    
+    return e
+
