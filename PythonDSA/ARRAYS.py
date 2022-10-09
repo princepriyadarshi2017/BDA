597bdeb8e71ec33e4c904d9b453e30122599caa4
Prince Priyadarshi <prince.priyadarshi@beatoapp.com>
2022-10-10 01:19:49 +0530

    first_commit

---
+++ PythonDSA/ARRAYS.py
@@ -0,0 +1,26 @@
+# Q1. Given an array A[] of n numbers and another number x, the task is to check whether 
+# or not there exist two elements in A[] whose sum is exactly x. 
+
+# Examples: 
+
+# Input: arr[] = {0, -1, 2, -3, 1}, x= -2
+# Output: Yes
+# Explanation:  If we calculate the sum of the output,1 + (-3) = -2
+
+# Input: arr[] = {1, -2, 1, 0, 5}, x = 0
+# Output: No
+
+def question1():
+    a=input()
+    a=a.split(",")
+    c=[]
+    for i in a:
+        c.append(int(i))
+    d=sum(c)
+    if d in c:
+        e="yes"
+    else:
+        e="no"
+
+    return e
+
