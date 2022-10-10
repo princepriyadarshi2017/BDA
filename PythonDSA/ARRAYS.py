3ee896cc394c9e18aa96a738a765d62f4dfdd120
Prince Priyadarshi <prince.priyadarshi@beatoapp.com>
2022-10-10 14:12:09 +0530

    innit

--- PythonDSA/ARRAYS.py
+++ PythonDSA/ARRAYS.py
@@ -53,3 +53,25 @@ def question2():
     
     return e
 
+# Q3 Given an array of positive integers. All numbers occur an even number of times except one number which occurs an odd number of times. Find the number in O(n) time & constant space.
+
+# Examples : 
+
+# Input : arr = {1, 2, 3, 2, 3, 1, 3}
+# Output : 3
+
+# Input : arr = {5, 7, 2, 7, 5, 2, 5}
+# Output : 5
+
+def question3(arr, arr_size):
+     
+    for i in range(0,arr_size):
+        count = 0
+        for j in range(0, arr_size):
+            if arr[i] == arr[j]:
+                count+=1
+             
+        if (count % 2 != 0):
+            return arr[i]
+         
+    return -1
