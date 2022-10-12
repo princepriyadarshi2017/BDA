dcb01c3a623632cd2d2189f7130bdfdd18fe1d52
Prince Priyadarshi <prince.priyadarshi@beatoapp.com>
2022-10-12 16:44:29 +0530

    iniit

--- Assigment 3PythonDSA/STRINGS.py
+++ Assigment 3PythonDSA/STRINGS.py
@@ -1,4 +1,7 @@
 #Q1 Remove duplicates from a given string
+from unittest import skip
+
+
 a=input()
 b=[]
 for i in a:
@@ -13,3 +16,58 @@ for i in remove:
 print(removedup)
 
 #Q2 Remove characters from the first string which are present in the second string
+a=input("First String:- ")
+b=input("second String:- ")
+a1=[]
+for i in a:
+    a1.append(i)
+b1=[]
+for i in b:
+    b1.append(i)
+c=[]
+for i in a1:
+    if i not in b1:
+        c.append(i)
+
+# Given a string s1 and a string s2, write a function to check whether s2 is a rotation of s1. 
+
+def check_rotation(s, goal):
+ 
+    if (len(s) != len(goal)):
+        skip
+ 
+    q1 = []
+    for i in range(len(s)):
+        q1.insert(0, s[i])
+ 
+    q2 = []
+    for i in range(len(goal)):
+        q2.insert(0, goal[i])
+ 
+    k = len(goal)
+    while (k > 0):
+        ch = q2[0]
+        q2.pop(0)
+        q2.append(ch)
+        if (q2 == q1):
+            return True
+ 
+        k -= 1
+
+# Write a program to print all permutations of a given string
+def toString(List): 
+    return ''.join(List) 
+  
+# Function to print permutations of string 
+# This function takes three parameters: 
+# 1. String 
+# 2. Starting index of the string 
+# 3. Ending index of the string. 
+def permute(a, l, r): 
+    if l==r: 
+        print (toString(a))
+    else: 
+        for i in range(l,r): 
+            a[l], a[i] = a[i], a[l] 
+            permute(a, l+1, r) 
+            a[l], a[i] = a[i], a[l] 
